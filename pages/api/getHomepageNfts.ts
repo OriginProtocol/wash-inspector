e34186fbf2590d93471ea196500329209d39fd64
Ed Spencer <ed@edspencer.net>
2023-01-13 18:44:14 -0500

    homepage data cache

--- pages/api/getHomepageNfts.ts
+++ pages/api/getHomepageNfts.ts
@@ -5,61 +5,77 @@ import collectionData from "../../data/collectionsdata";
 import useAlchemy from "../../src/hooks/useAlchemy";
 const fs = require("fs");
 
+let cache: any = false;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    //let { generate } = req.query;
-    res.status(200).json({ result: (await generateNFTAggregate()) });
+  //let { generate } = req.query;
+  if (!cache) {
+    cache = await generateNFTAggregate();
+  }
+  res.status(200).json({ result: cache });
 }
 
-async function generateNFTAggregate(){
+async function generateNFTAggregate() {
   const alchemy = useAlchemy();
-  const files = fs.readdirSync(`${process.cwd()}/data/collectionAnalysis/`)
+  const files = fs.readdirSync(`${process.cwd()}/data/collectionAnalysis/`);
 
-  let totalTrades = 0
-  let totalWashTrades = 0
-  let totalVolume = 0
-  let totalWashVolume = 0
-  let nftProjects = []
+  let totalTrades = 0;
+  let totalWashTrades = 0;
+  let totalVolume = 0;
+  let totalWashVolume = 0;
+  let nftProjects = [];
 
-  await Promise.all(files.map(async function (file) {
-    const fileBuffer = fs.readFileSync(`${process.cwd()}/data/collectionAnalysis/${file}`);
-    const json = JSON.parse(fileBuffer)
+  await Promise.all(
+    files.map(async function (file) {
+      const fileBuffer = fs.readFileSync(
+        `${process.cwd()}/data/collectionAnalysis/${file}`
+      );
+      const json = JSON.parse(fileBuffer);
 
-    const {trades, washTrades, volume, washVolume, address, washTradedNfts} = json
-    totalTrades += trades
-    totalWashTrades += washTrades
-    totalVolume += volume
-    totalWashVolume += washVolume
+      const {
+        trades,
+        washTrades,
+        volume,
+        washVolume,
+        address,
+        washTradedNfts,
+      } = json;
+      totalTrades += trades;
+      totalWashTrades += washTrades;
+      totalVolume += volume;
+      totalWashVolume += washVolume;
 
-    const metadata = await alchemy.nft.getContractMetadata(address);
+      const metadata = await alchemy.nft.getContractMetadata(address);
 
-    nftProjects.push({
-      address,
-      name: metadata.name,
-      imageUrl: metadata.openSea.imageUrl,
-      externalUrl: metadata.openSea.externalUrl,
-      description: metadata.openSea.description,
-      trades,
-      washTrades,
-      volume,
-      washVolume,
-      nftsWashTradedCount: washTradedNfts.length,
-      washTradedNftsAggregates: countBy(washTradedNfts.flatMap(nft => nft.washTrades))
+      nftProjects.push({
+        address,
+        name: metadata.name,
+        imageUrl: metadata.openSea.imageUrl,
+        externalUrl: metadata.openSea.externalUrl,
+        description: metadata.openSea.description,
+        trades,
+        washTrades,
+        volume,
+        washVolume,
+        nftsWashTradedCount: washTradedNfts.length,
+        washTradedNftsAggregates: countBy(
+          washTradedNfts.flatMap((nft) => nft.washTrades)
+        ),
+      });
     })
-  }));
+  );
 
   return {
     totalTrades,
     totalWashTrades,
     totalVolume,
     totalWashVolume,
-    nftProjects: orderBy(nftProjects, ["washTrades"], ["desc"])
+    nftProjects: orderBy(nftProjects, ["washTrades"], ["desc"]),
   };
 }
-
-
 
 // fs.writeFileSync(
 //     `${process.cwd()}/data/collectionAnalysis/${address.toLowerCase()}.json`,
